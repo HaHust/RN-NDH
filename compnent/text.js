95a5402c909a6ae09f6a83f12d73f1619441265e
HaHust <vanha.hust@gmail.com>
2020-02-04 08:40:21 +0700

    first commit

---
+++ compnent/text.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert,
+    Platform, TouchableHighlight, Dimensions,
+    TextInput
+} from 'react-native';
+export default class Haha extends Component{
+    render(){
+        return(
+            <Text>haha</Text>
+        )
+    }
+}
---
+++ data/flatListData.js
@@ -0,0 +1,27 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+FlatList input data
+*/
+var flatListData = [{"id":"2a0e698c-03d9-42db-9153-104052068c0d","name":"Merci Petters","email":"mpetters0@army.mil","phoneNumber":"+46 732 654 3992","img":"https://loremflickr.com/320/240/dog"},
+{"id":"0b35734c-4845-4700-b9f4-f2c66f013e64","name":"Halli Tumber","email":"htumber1@webeden.co.uk","phoneNumber":"+63 943 206 2156","img":"https://loremflickr.com/320/240/dog"},
+{"id":"727fc934-4e7f-411a-8813-87e73862912d","name":"Portie Bastone","email":"pbastone2@jiathis.com","phoneNumber":"+856 327 266 2350","img":"https://loremflickr.com/320/240/dog"},
+{"id":"6a47e723-a6f7-47f6-87ce-aed34d6be22b","name":"Grace Muckian","email":"gmuckian3@shop-pro.jp","phoneNumber":"+994 990 859 4008","img":"https://loremflickr.com/320/240/dog"},
+{"id":"f3552ecd-664a-4edb-8b4c-ce9c9e9476a8","name":"Hardy Amoore","email":"hamoore4@ifeng.com","phoneNumber":"+261 642 203 5218","img":"https://loremflickr.com/320/240/dog"},
+{"id":"b4b4524d-6a77-46b4-9ddd-e894615d9bde","name":"Delila Gage","email":"dgage5@nih.gov","phoneNumber":"+48 322 617 1060","img":"https://loremflickr.com/320/240/dog"},
+{"id":"62cfc4aa-390a-43fa-a42d-edd338b18aad","name":"Celia Mitkov","email":"cmitkov6@ask.com","phoneNumber":"+86 351 107 2604","img":"https://loremflickr.com/320/240/dog"},
+{"id":"b748da3a-01cf-49a6-8a01-3df4e55e00f5","name":"Keri Moroney","email":"kmoroney7@weibo.com","phoneNumber":"+62 191 401 8441"},
+{"id":"0fc1e0ab-9257-4a7a-9acf-a3854984ed48","name":"Marmaduke Crome","email":"mcrome8@networksolutions.com","phoneNumber":"+687 854 876 0377"},
+{"id":"d839804e-90d6-48ff-ac2b-fac346525d2f","name":"Keelby Winsborrow","email":"kwinsborrow9@elegantthemes.com","phoneNumber":"+54 309 748 2840"},
+{"id":"f9b22ac0-e1d2-4643-a872-7c07b7b2bc7f","name":"Penni Surgey","email":"psurgeya@china.com.cn","phoneNumber":"+234 531 974 7191"},
+{"id":"63fccf16-a2a4-4588-8c34-a732d275fd47","name":"Mercy Margetts","email":"mmargettsb@ycombinator.com","phoneNumber":"+251 805 879 8500"},
+{"id":"3d7e6b34-3e46-47c4-957e-0338e2e299f5","name":"Loretta Bratton","email":"lbrattonc@usgs.gov","phoneNumber":"+30 354 475 3574"},
+{"id":"540bb404-89f6-4f6b-a7e3-5e3e4dc666d1","name":"Cheryl Patton","email":"cpattond@techcrunch.com","phoneNumber":"+86 377 882 0843"},
+{"id":"ce30cbdc-5990-4ebe-a13f-a5028c87756d","name":"Tymon MacTeague","email":"tmacteaguee@liveinternet.ru","phoneNumber":"+86 950 304 1779"},
+{"id":"1755a86b-ba78-4b2a-9b26-ff65c1412c62","name":"Romola Arnley","email":"rarnleyf@globo.com","phoneNumber":"+54 245 876 5757"},
+{"id":"79e3270e-2c07-4c33-b065-3363c736fac6","name":"Sibel Alessandretti","email":"salessandrettig@livejournal.com","phoneNumber":"+351 956 433 4106"},
+{"id":"92285fcc-3eb9-4bf9-92ed-ac63f5192b62","name":"Bret Bridgestock","email":"bbridgestockh@tumblr.com","phoneNumber":"+62 531 671 4272"},
+{"id":"d14dd0e0-4c93-4dea-a46a-aa021781fb5d","name":"North Burel","email":"nbureli@ucoz.com","phoneNumber":"+47 500 720 6837"},
+{"id":"5ec42cc3-1a94-4670-a142-25bddf361197","name":"Sollie Clever","email":"scleverj@acquirethisname.com","phoneNumber":"+221 413 823 0829"}]
+module.exports = flatListData;
